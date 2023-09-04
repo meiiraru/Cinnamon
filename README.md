6cc623132f4c9a3aa8fb104939e51c4766ac0dac
May <141885264+sheep-may@users.noreply.github.com>
2023-09-04 07:48:52 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Controls
+* Escape: exit
+* Left-Alt: release mouse controls
+#
+* WASD: move
+* Space: move up
+* Left-Shift: move down
+* TAB: sprint
+#
+* Mouse: rotate camera
+* Left-Click: break
+* Right-Click: place
+* Middle-Click: pick block
+* Scroll: change hotbar slot
+#
+* 1-9: select hotbar slot
+#
+* Z: toggle light rotation
+* X: move light to camera (must not be rotating)
+#
+* F1-F4: load save slot
+#
+* F5: filled renderer
+* F6: lines renderer
---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.lwjgl.Lwjgl
+import org.lwjgl.Lwjgl.implementation
+import org.lwjgl.sonatype
+
+plugins {
+    id("java")
+    id("org.lwjgl.plugin") version "0.0.34"
+}
+
+group = "io.github.sheep_may"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    sonatype()
+}
+
+dependencies {
+    implementation(Lwjgl.Preset.everything)
+    implementation("org.joml:joml:1.10.5")
+}
+
+tasks.jar {
+    from(configurations.runtimeClasspath.get().map(::zipTree))
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from("LICENSE.md")
+
+    manifest.attributes["Main-Class"] = "mayo.Main"
+}
