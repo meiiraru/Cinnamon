d9227cfea9ce9cef650685aafeb2f59500a6ecbb
woolfy <lucasrodriguesruiz@gmail.com>
2024-05-16 20:35:08 -0300

    Fixed the posterization shader to effect saturation and value components instead of red, green and blue components.

--- src/main/resources/resources/vanilla/shaders/post/bits.glsl
+++ src/main/resources/resources/vanilla/shaders/post/bits.glsl
@@ -11,9 +11,32 @@ out vec4 fragColor;
 uniform sampler2D colorTex;
 uniform int bits;
 
+// All components are in the range [0…1], including hue.
+vec3 rgb2hsv(vec3 c)
+{
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+// All components are in the range [0…1], including hue.
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+
 void main() {
     float bit = pow(2.0f, bits);
     vec4 color = texture(colorTex, texCoords);
-    color = floor(color * bit) / bit;
+    color = vec4(rgb2hsv(color.rgb), color.a);
+    color.gb = floor(color.gb * bit) / bit;
+    color = vec4(hsv2rgb(color.rgb), color.a);
     fragColor = color;
 }
