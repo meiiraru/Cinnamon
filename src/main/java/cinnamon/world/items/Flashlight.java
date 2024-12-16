146b248e7871aac21f201bdff6d3e2ea03e84eb5
Lucas Ruiz <lucasrodriguesruiz@gmail.com>
2024-12-16 17:49:00 -0300

    Added new sound to Flashlight

--- src/main/java/cinnamon/world/items/Flashlight.java
+++ src/main/java/cinnamon/world/items/Flashlight.java
@@ -2,12 +2,17 @@ package cinnamon.world.items;
 
 import cinnamon.registry.ItemModelRegistry;
 import cinnamon.render.MatrixStack;
+import cinnamon.sound.SoundCategory;
+import cinnamon.utils.Maths;
+import cinnamon.utils.Resource;
 import cinnamon.world.entity.Entity;
 import cinnamon.world.light.Spotlight;
 import cinnamon.world.world.World;
 import cinnamon.world.world.WorldClient;
 
 public class Flashlight extends Item {
+
+    private static final Resource USE_SOUND = new Resource("sounds/item.flashlight/switch.ogg");
 
     private static final int DELAY_TO_USE = 10;
 
@@ -49,6 +54,8 @@ public class Flashlight extends Item {
 
         delay = DELAY_TO_USE;
         active = !active;
+
+        source.getWorld().playSound(USE_SOUND, SoundCategory.ENTITY, source.getPos());
 
         World world = owner.getWorld();
         if (world.isClientside()) {
