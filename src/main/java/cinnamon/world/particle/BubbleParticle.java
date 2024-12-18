22a9faa596e2a18e91eca0fd739b1143211ad6fd
Meii <141885264+meiiraru@users.noreply.github.com>
2024-12-18 00:58:11 -0300

    Revert "Switched bubble sound for a group of sounds to make it sound better"
    This reverts commit 001428513d00347fdb114ac46f866cd065c374c4.

--- src/main/java/cinnamon/world/particle/BubbleParticle.java
+++ src/main/java/cinnamon/world/particle/BubbleParticle.java
@@ -8,11 +8,7 @@ import cinnamon.utils.Resource;
 
 public class BubbleParticle extends SpriteParticle {
 
-    private static final Resource[] POP_SOUND = {
-            new Resource("sounds/particle/bubble/pop/1.ogg"),
-            new Resource("sounds/particle/bubble/pop/2.ogg"),
-            new Resource("sounds/particle/bubble/pop/3.ogg")
-    };
+    private static final Resource POP_SOUND = new Resource("sounds/bubble_pop.ogg");
 
     public BubbleParticle(int lifetime, int color) {
         super(lifetime, color);
@@ -46,9 +42,7 @@ public class BubbleParticle extends SpriteParticle {
     @Override
     public void remove() {
         super.remove();
-        int index = (int) (Math.random() * POP_SOUND.length);
-        Resource sound = POP_SOUND[index];
-        world.playSound(sound, SoundCategory.AMBIENT, pos).volume(0.25f).pitch(Maths.range(0.8f, 1.2f));
+        world.playSound(POP_SOUND, SoundCategory.AMBIENT, pos).volume(0.1f).pitch(Maths.range(0.8f, 1.2f));
     }
 
     @Override
