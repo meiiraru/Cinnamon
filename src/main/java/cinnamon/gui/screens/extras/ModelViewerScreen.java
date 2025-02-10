6cbf1f7df97b5fdc360be08c1b71b15bd6d6a711
Meii <141885264+meiiraru@users.noreply.github.com>
2025-02-10 15:00:22 -0300

    model viewer widget
    * created a separated widget for the model viewer, allowing for better instancing and control of the viewer

--- src/main/java/cinnamon/gui/screens/extras/ModelViewerScreen.java
+++ src/main/java/cinnamon/gui/screens/extras/ModelViewerScreen.java
@@ -7,55 +7,32 @@ import cinnamon.gui.widgets.WidgetList;
 import cinnamon.gui.widgets.types.Button;
 import cinnamon.gui.widgets.types.ComboBox;
 import cinnamon.gui.widgets.types.Label;
-import cinnamon.model.GeometryHelper;
-import cinnamon.model.ModelManager;
+import cinnamon.gui.widgets.types.ModelViewer;
 import cinnamon.registry.*;
 import cinnamon.render.MatrixStack;
 import cinnamon.render.batch.VertexConsumer;
-import cinnamon.render.framebuffer.Framebuffer;
-import cinnamon.render.model.AnimatedObjRenderer;
-import cinnamon.render.model.ModelRenderer;
-import cinnamon.render.shader.Shader;
-import cinnamon.render.shader.Shaders;
-import cinnamon.render.texture.Texture;
 import cinnamon.text.Style;
 import cinnamon.text.Text;
-import cinnamon.utils.*;
+import cinnamon.utils.Alignment;
+import cinnamon.utils.Resource;
 import cinnamon.world.SkyBox;
 
+import java.util.List;
 import java.util.function.BiFunction;
 
 import static cinnamon.Client.LOGGER;
-import static org.lwjgl.glfw.GLFW.*;
 
 public class ModelViewerScreen extends ParentedScreen {
 
     private static final int listWidth = 144;
-    private static final float scaleFactor = 0.1f;
-
-    private final Framebuffer modelBuffer = new Framebuffer(1, 1, Framebuffer.COLOR_BUFFER | Framebuffer.DEPTH_BUFFER);
-    private final SkyBox skybox = new SkyBox();
 
     //current opened model
-    private ModelRenderer currentModel = null;
-    private String modelName = "none";
-    private MaterialRegistry selectedMaterial = MaterialRegistry.DEFAULT;
+    private String modelName = "";
     private final ComboBox animationList = new ComboBox(0, 0, 60, 14);
-
-    //view transforms
-    private float posX = 0, posY = 0;
-    private float scale = 128, scaleReset = 128;
-    private float rotX = -22.5f, rotY = 30;
-
-    //dragging
-    private int dragged = -1;
-    private int anchorX = 0, anchorY = 0;
-    private float anchorRotX = 0, anchorRotY = 0;
-    private float anchorPosX = 0, anchorPosY = 0;
+    private final ModelViewer modelViewer = new ModelViewer(0, 0, 1, 1);
 
     public ModelViewerScreen(Screen parentScreen) {
         super(parentScreen);
-        skybox.type = SkyBox.Type.WHITE;
     }
 
     @Override
@@ -95,9 +72,9 @@ public class ModelViewerScreen extends ParentedScreen {
         //add material list
         ComboBox materials = new ComboBox(width - animationList.getWidth() - 4, 4, animationList.getWidth(), animationList.getHeight());
         for (MaterialRegistry value : MaterialRegistry.values())
-            materials.addEntry(Text.of(value.name()), null, b -> selectedMaterial = value);
+            materials.addEntry(Text.of(value.name()), null, b -> modelViewer.setMaterial(value));
         materials.setTooltip(Text.of("Materials"));
-        materials.select(selectedMaterial.ordinal());
+        materials.select(modelViewer.getMaterial().ordinal());
         addWidget(materials);
 
         //prepare animations list
@@ -108,40 +85,27 @@ public class ModelViewerScreen extends ParentedScreen {
         //skyboxes
         ComboBox skyboxes = new ComboBox(materials.getX(), animationList.getY() + animationList.getHeight() + 4, animationList.getWidth(), animationList.getHeight());
         for (SkyBox.Type value : SkyBox.Type.values())
-            skyboxes.addEntry(Text.of(value.name()), null, b -> skybox.type = value);
+            skyboxes.addEntry(Text.of(value.name()), null, b -> modelViewer.setSkybox(value));
         skyboxes.setTooltip(Text.of("Skybox"));
-        skyboxes.select(skybox.type.ordinal());
+        skyboxes.select(modelViewer.getSkybox().ordinal());
         addWidget(skyboxes);
+
+        //add model viewer
+        modelViewer.setPos(listWidth + 4, 4);
+        modelViewer.setDimensions(width - listWidth - 8, height - 8);
 
         super.init();
 
         //set initial model
-        if (currentModel == null)
+        if (!modelViewer.hasModel())
             setModel(LivingModelRegistry.STRAWBERRY.resource, LivingModelRegistry.STRAWBERRY.name());
-
-        modelBuffer.resize(client.window.width, client.window.height);
     }
 
     @Override
-    public void removed() {
-        super.removed();
-        modelBuffer.free();
-    }
-
-    @Override
-    protected void preRender(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        super.preRender(matrices, mouseX, mouseY, delta);
-
-        //render model bg
-        //VertexConsumer.GUI.consume(GeometryHelper.rectangle(matrices, listWidth, 4, width - 4, height - 4, 0x20000000));
-
-        //render model
-        if (client.anaglyph3D) {
-            client.camera.anaglyph3D(matrices, 1f, 1f, () -> renderModelToBuffer(matrices), () -> renderBuffer(matrices));
-        } else {
-            renderModelToBuffer(matrices);
-            renderBuffer(matrices);
-        }
+    protected void addBackButton() {
+        super.addBackButton();
+        //add last of the last
+        addWidget(modelViewer);
     }
 
     @Override
@@ -152,177 +116,23 @@ public class ModelViewerScreen extends ParentedScreen {
         font.render(VertexConsumer.FONT, matrices, (width - listWidth) / 2f + listWidth, 4, Text.of(modelName).withStyle(Style.EMPTY.outlined(true)), Alignment.CENTER);
     }
 
-    private void renderModelToBuffer(MatrixStack matrices) {
-        if (currentModel == null)
-            return;
-
-        //prepare render
-        Shader oldShader = Shader.activeShader;
-        client.camera.useOrtho(false);
-        matrices.push();
-
-        //position
-        matrices.translate(posX, -posY, -200);
-
-        //scale
-        matrices.scale(scale, scale, scale);
-
-        //rotate model
-        matrices.rotate(Rotation.X.rotationDeg(-rotX));
-        matrices.rotate(Rotation.Y.rotationDeg(-rotY - 180));
-
-        //offset model to center
-        matrices.translate(currentModel.getAABB().getCenter().mul(-1f));
-
-        //setup shader
-        Shader s = Shaders.WORLD_MODEL_PBR.getShader().use();
-        s.setup(client.camera);
-        s.setVec3("camPos", 0, 0, 0);
-        s.setFloat("fogStart", 1024);
-        s.setFloat("fogEnd", 2048);
-        skybox.pushToShader(s, Texture.MAX_TEXTURES - 1);
-
-        //render to another framebuffer
-        modelBuffer.useClear();
-        currentModel.render(matrices, selectedMaterial.material);
-        Framebuffer.DEFAULT_FRAMEBUFFER.use();
-
-        //cleanup
-        oldShader.use();
-        matrices.pop();
-        client.camera.useOrtho(true);
-    }
-
-    private void renderBuffer(MatrixStack matrices) {
-        //draw framebuffer result
-        UIHelper.pushScissors(listWidth, 4, width - listWidth - 4, height - 8);
-        VertexConsumer.GUI.consume(GeometryHelper.quad(matrices, listWidth / 2f, 0, width, height, 0, 0f, 1f, 1f, 0f), modelBuffer.getColorBuffer());
-        UIHelper.popScissors();
-    }
-
     private void setModel(Resource model, String name) {
-        currentModel = ModelManager.load(model);
+        modelViewer.setModel(model);
         modelName = name;
 
         animationList.clearEntries();
-        if (currentModel instanceof AnimatedObjRenderer animModel) {
-            animationList.addEntry(Text.of("None"), null, b -> animModel.stopAllAnimations());
-            for (String animation : animModel.getAnimations())
+        List<String> animations = modelViewer.getAnimations();
+        if (!animations.isEmpty()) {
+            animationList.addEntry(Text.of("None"), null, b -> modelViewer.stopAllAnimations());
+            for (String animation : animations)
                 animationList.addEntry(Text.of(animation), null, b -> {
-                    animModel.stopAllAnimations();
-                    animModel.getAnimation(animation).setLoop(Animation.Loop.LOOP).play();
+                    modelViewer.stopAllAnimations();
+                    modelViewer.getAnimation(animation).setLoop(Animation.Loop.LOOP).play();
                 });
         } else {
             animationList.addEntry(Text.of("None"), null, null);
         }
         animationList.select(0);
-
-        float maxDimension = Maths.max(currentModel.getAABB().getDimensions());
-        scaleReset = 128 / maxDimension;
-        resetView();
-    }
-
-    private void resetView() {
-        posX = 0;
-        posY = 0;
-        scale = scaleReset;
-        rotX = -22.5f;
-        rotY = 30;
-    }
-
-    @Override
-    public boolean mousePress(int button, int action, int mods) {
-        if (dragged != -1) {
-            dragged = -1;
-            return true;
-        }
-
-        boolean sup = super.mousePress(button, action, mods);
-        if (sup) return true;
-
-        int mouseX = client.window.mouseX;
-        int mouseY = client.window.mouseY;
-
-        if (currentModel == null || !UIHelper.isMouseOver(listWidth, 4, width - listWidth - 4, height - 8, mouseX, mouseY))
-            return false;
-
-        dragged = button;
-        anchorX = mouseX;
-        anchorY = mouseY;
-
-        switch (button) {
-            case GLFW_MOUSE_BUTTON_1 -> {
-                anchorRotX = rotX;
-                anchorRotY = rotY;
-            }
-            case GLFW_MOUSE_BUTTON_2 -> {
-                anchorPosX = posX;
-                anchorPosY = posY;
-            }
-            case GLFW_MOUSE_BUTTON_3 -> resetView();
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean mouseMove(int x, int y) {
-        if (dragged == -1)
-            return super.mouseMove(x, y);
-
-        float dx = x - anchorX;
-        float dy = y - anchorY;
-
-        if (dragged == GLFW_MOUSE_BUTTON_1) {
-            rotX = anchorRotX - dy;
-            rotY = anchorRotY - dx;
-        } else if (dragged == GLFW_MOUSE_BUTTON_2) {
-            posX = anchorPosX + dx;
-            posY = anchorPosY + dy;
-
-            //limit to boundaries
-            float limitX = (width - listWidth - 4) / 2f;
-            float limitY = (height - 4) / 2f;
-            posX = Math.clamp(posX, -limitX, limitX);
-            posY = Math.clamp(posY, -limitY, limitY);
-        }
-
-        client.window.warpMouse((deltaX, deltaY) -> {
-            anchorX += deltaX;
-            anchorY += deltaY;
-        });
-
-        return true;
-    }
-
-    @Override
-    public boolean scroll(double x, double y) {
-        boolean sup = super.scroll(x, y);
-        if (sup) return true;
-
-        if (currentModel == null)
-            return false;
-
-        scale *= 1f + (float) Math.signum(y) * scaleFactor;
-
-        /*
-        //zoom around the mouse position
-        float mx = client.window.mouseX - (width - listWidth) / 2f - listWidth;
-        float my = client.window.mouseY - height / 2f;
-        float oldScale = scale;
-        scale = Math.max(scale + (float) Math.signum(y) * scaleFactor, 16f);
-        float scaleDiff = scale - oldScale;
-        posX -= mx * scaleDiff / oldScale;
-        posY -= my * scaleDiff / oldScale;
-        */
-
-        return true;
-    }
-
-    @Override
-    public boolean windowFocused(boolean focused) {
-        if (!focused) dragged = -1;
-        return super.windowFocused(focused);
     }
 
     public boolean filesDropped(String[] files) {
---
+++ src/main/java/cinnamon/gui/widgets/types/ModelViewer.java
@@ -0,0 +1,298 @@
+package cinnamon.gui.widgets.types;
+
+import cinnamon.Client;
+import cinnamon.animation.Animation;
+import cinnamon.gui.widgets.GUIListener;
+import cinnamon.gui.widgets.SelectableWidget;
+import cinnamon.model.GeometryHelper;
+import cinnamon.model.ModelManager;
+import cinnamon.registry.MaterialRegistry;
+import cinnamon.render.MatrixStack;
+import cinnamon.render.Window;
+import cinnamon.render.batch.VertexConsumer;
+import cinnamon.render.framebuffer.Framebuffer;
+import cinnamon.render.model.AnimatedObjRenderer;
+import cinnamon.render.model.ModelRenderer;
+import cinnamon.render.shader.Shader;
+import cinnamon.render.shader.Shaders;
+import cinnamon.render.texture.Texture;
+import cinnamon.utils.Maths;
+import cinnamon.utils.Resource;
+import cinnamon.utils.Rotation;
+import cinnamon.world.SkyBox;
+
+import java.util.List;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+
+public class ModelViewer extends SelectableWidget {
+
+    private static final Framebuffer modelBuffer = new Framebuffer(1, 1, Framebuffer.COLOR_BUFFER | Framebuffer.DEPTH_BUFFER);
+    private static final SkyBox the_skybox = new SkyBox();
+
+    //properties
+    private ModelRenderer model = null;
+    private MaterialRegistry selectedMaterial = MaterialRegistry.DEFAULT;
+    private SkyBox.Type skybox = SkyBox.Type.WHITE;
+
+    private float defaultScale = 128f, scaleFactor = 0.1f;
+    private float defaultRotX = -22.5f, defaultRotY = 30f;
+    private boolean flipX, flipY, flipZ;
+
+    //view transforms
+    private float posX = 0, posY = 0;
+    private float scale = defaultScale, scaleReset = defaultScale;
+    private float rotX = defaultRotX, rotY = defaultRotY;
+
+    //dragging
+    private int dragged = -1;
+    private int anchorX = 0, anchorY = 0;
+    private float anchorRotX = 0, anchorRotY = 0;
+    private float anchorPosX = 0, anchorPosY = 0;
+
+    public ModelViewer(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    public static void free() {
+        modelBuffer.free();
+    }
+
+    @Override
+    public void renderWidget(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        //render model
+        Client client = Client.getInstance();
+        if (client.anaglyph3D) {
+            client.camera.anaglyph3D(matrices, 1f, 1f, () -> renderModelToBuffer(matrices), () -> renderBuffer(matrices));
+        } else {
+            renderModelToBuffer(matrices);
+            renderBuffer(matrices);
+        }
+    }
+
+    private void renderModelToBuffer(MatrixStack matrices) {
+        if (model == null)
+            return;
+
+        //prepare render
+        Shader oldShader = Shader.activeShader;
+        Client client = Client.getInstance();
+        client.camera.useOrtho(false);
+        matrices.push();
+
+        //position
+        matrices.translate(posX, -posY, -200);
+
+        //scale
+        matrices.scale(scale, scale, scale);
+
+        //rotate model
+        matrices.rotate(Rotation.X.rotationDeg(-rotX));
+        matrices.rotate(Rotation.Y.rotationDeg(-rotY - 180));
+
+        //offset model to center
+        matrices.translate(model.getAABB().getCenter().mul(-1f));
+
+        //setup shader
+        Shader s = Shaders.WORLD_MODEL_PBR.getShader().use();
+        s.setup(client.camera);
+        s.setVec3("camPos", 0, 0, 0);
+        s.setFloat("fogStart", 1024);
+        s.setFloat("fogEnd", 2048);
+        the_skybox.type = skybox;
+        the_skybox.pushToShader(s, Texture.MAX_TEXTURES - 1);
+
+        //apply model flip
+        matrices.scale(flipX ? -1 : 1, flipY ? -1 : 1, flipZ ? -1 : 1);
+        if (flipX ^ flipY ^ flipZ)
+            glCullFace(GL_FRONT);
+        if (flipY)
+            matrices.translate(0, -model.getAABB().getHeight(), 0);
+
+        //render to another framebuffer;
+        modelBuffer.useClear();
+        modelBuffer.resizeTo(Framebuffer.DEFAULT_FRAMEBUFFER);
+        model.render(matrices, selectedMaterial.material);
+        Framebuffer.DEFAULT_FRAMEBUFFER.use();
+
+        //cleanup
+        glCullFace(GL_BACK);
+        oldShader.use();
+        matrices.pop();
+        client.camera.useOrtho(true);
+    }
+
+    private void renderBuffer(MatrixStack matrices) {
+        //draw framebuffer result
+        float guiScale = Client.getInstance().window.guiScale;
+        float x = (1f - (getWidth() * guiScale) / modelBuffer.getWidth()) / 2f;
+        float y = (1f - (getHeight() * guiScale) / modelBuffer.getHeight()) / 2f;
+        VertexConsumer.GUI.consume(GeometryHelper.quad(matrices, getX(), getY(), getWidth(), getHeight(), -1f, x, 1f - x, 1f - y, y), modelBuffer.getColorBuffer());
+    }
+
+    public void setMaterial(MaterialRegistry material) {
+        this.selectedMaterial = material;
+    }
+
+    public void setSkybox(SkyBox.Type type) {
+        skybox = type;
+    }
+
+    public MaterialRegistry getMaterial() {
+        return selectedMaterial;
+    }
+
+    public SkyBox.Type getSkybox() {
+        return skybox;
+    }
+
+    public boolean hasModel() {
+        return model != null;
+    }
+
+    public void setModel(Resource res) {
+        model = ModelManager.load(res);
+        float maxDimension = Maths.max(model.getAABB().getDimensions());
+        scaleReset = defaultScale / maxDimension;
+        resetView();
+    }
+
+    public float getRotX() {
+        return rotX;
+    }
+
+    public float getRotY() {
+        return rotY;
+    }
+
+    public void setDefaultRot(float rotX, float rotY) {
+        this.defaultRotX = rotX;
+        this.defaultRotY = rotY;
+    }
+
+    public void setDefaultScale(float scale) {
+        this.defaultScale = scale;
+    }
+
+    public void setScaleFactor(float scaleFactor) {
+        this.scaleFactor = scaleFactor;
+    }
+
+    public void setModelFlip(boolean flipX, boolean flipY, boolean flipZ) {
+        this.flipX = flipX;
+        this.flipY = flipY;
+        this.flipZ = flipZ;
+    }
+
+    private void resetView() {
+        posX = 0;
+        posY = 0;
+        scale = scaleReset;
+        rotX = defaultRotX;
+        rotY = defaultRotY;
+    }
+
+    public List<String> getAnimations() {
+        if (model instanceof AnimatedObjRenderer animModel)
+            return animModel.getAnimations();
+        return List.of();
+    }
+
+    public void stopAllAnimations() {
+        if (model instanceof AnimatedObjRenderer animModel)
+            animModel.stopAllAnimations();
+    }
+
+    public Animation getAnimation(String anim) {
+        if (model instanceof AnimatedObjRenderer animModel)
+            return animModel.getAnimation(anim);
+        return null;
+    }
+
+    @Override
+    public GUIListener mousePress(int button, int action, int mods) {
+        if (dragged != -1) {
+            dragged = -1;
+            return this;
+        }
+
+        if (model == null || !isHovered() || action != GLFW_PRESS) {
+            dragged = -1;
+            return null;
+        }
+
+        Window w = Client.getInstance().window;
+        dragged = button;
+        anchorX = w.mouseX;
+        anchorY = w.mouseY;
+
+        switch (button) {
+            case GLFW_MOUSE_BUTTON_1 -> {
+                anchorRotX = rotX;
+                anchorRotY = rotY;
+            }
+            case GLFW_MOUSE_BUTTON_2 -> {
+                anchorPosX = posX;
+                anchorPosY = posY;
+            }
+            case GLFW_MOUSE_BUTTON_3 -> resetView();
+        }
+
+        return this;
+    }
+
+    @Override
+    public GUIListener mouseMove(int x, int y) {
+        GUIListener sup = super.mouseMove(x, y);
+        if (sup != null) return sup;
+
+        if (dragged == -1)
+            return null;
+
+        float dx = x - anchorX;
+        float dy = y - anchorY;
+
+        if (dragged == GLFW_MOUSE_BUTTON_1) {
+            rotX = anchorRotX - dy;
+            rotY = anchorRotY - dx;
+        } else if (dragged == GLFW_MOUSE_BUTTON_2) {
+            posX = anchorPosX + dx;
+            posY = anchorPosY + dy;
+        }
+
+        Client.getInstance().window.warpMouse((deltaX, deltaY) -> {
+            anchorX += deltaX;
+            anchorY += deltaY;
+        });
+
+        return this;
+    }
+
+    @Override
+    public GUIListener scroll(double x, double y) {
+        if (model == null)
+            return null;
+
+        scale *= 1f + (float) Math.signum(y) * scaleFactor;
+
+        /*
+        //zoom around the mouse position
+        float mx = client.window.mouseX - (width - listWidth) / 2f - listWidth;
+        float my = client.window.mouseY - height / 2f;
+        float oldScale = scale;
+        scale = Math.max(scale + (float) Math.signum(y) * scaleFactor, 16f);
+        float scaleDiff = scale - oldScale;
+        posX -= mx * scaleDiff / oldScale;
+        posY -= my * scaleDiff / oldScale;
+        */
+
+        return this;
+    }
+
+    @Override
+    public GUIListener windowFocused(boolean focused) {
+        if (!focused) dragged = -1;
+        return super.windowFocused(focused);
+    }
+}
