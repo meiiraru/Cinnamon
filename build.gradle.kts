58d9cefc15f68c1c9903abb3c0d717c90995ba35
Meii <141885264+meiiraru@users.noreply.github.com>
2025-11-24 18:35:16 -0300

    reworked args options
    * reworked args options
    * forgor to bump version to 0.3.9 last time

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.meiiraru"
-version = "0.3.8"
+version = "0.3.9"
 val mainClass = "cinnamon.Cinnamon"
 
 //dependencies
--- src/main/java/cinnamon/settings/ArgsOptions.java
+++ src/main/java/cinnamon/settings/ArgsOptions.java
@@ -3,6 +3,9 @@ package cinnamon.settings;
 import cinnamon.utils.Version;
 import org.joml.Math;
 
+import java.util.Arrays;
+import java.util.function.Predicate;
+
 public enum ArgsOptions {
     //special case for help and version
     HELP(null, "-h", "--help"),
@@ -22,36 +25,53 @@ public enum ArgsOptions {
     RENDER_DOC("", "--render-doc");
 
     private final String[] aliases;
-    private final String defaultValue;
-    private String value;
+    private final int argCount;
+    private final Predicate<String> predicate;
+    private final Object defaultValue;
+    private Object value;
 
     ArgsOptions(Object defaultValue, String... aliases) {
-        this.defaultValue = this.value = defaultValue == null ? null : defaultValue.toString();
+        this(defaultValue, obj -> true, aliases);
+    }
+
+    ArgsOptions(Object defaultValue, Predicate<String> predicate, String... aliases) {
         this.aliases = aliases;
+        this.argCount = defaultValue == null ? 0 : (defaultValue instanceof Object[] arr ? arr.length : 1);
+        this.predicate = predicate;
+        this.defaultValue = defaultValue;
+        this.value = defaultValue instanceof Object[] arr ? Arrays.copyOf(arr, arr.length) : defaultValue;
     }
 
     public boolean getAsBool() {
-        return value != null;
+        return Boolean.parseBoolean(String.valueOf(value));
+    }
+
+    public boolean getAsBool(int i) {
+        return Boolean.parseBoolean(String.valueOf(((Object[]) value)[i]));
     }
 
     public int getAsInt() {
-        try {
-            return Integer.parseInt(value);
-        } catch (Exception ignored) {
-            return Integer.parseInt(defaultValue);
-        }
+        return Integer.parseInt(String.valueOf(value));
+    }
+
+    public int getAsInt(int i) {
+        return Integer.parseInt(String.valueOf(((Object[]) value)[i]));
     }
 
     public float getAsFloat() {
-        try {
-            return Float.parseFloat(value);
-        } catch (Exception ignored) {
-            return Float.parseFloat(defaultValue);
-        }
+        return Float.parseFloat(String.valueOf(value));
+    }
+
+    public float getAsFloat(int i) {
+        return Float.parseFloat(String.valueOf(((Object[]) value)[i]));
     }
 
     public String getAsString() {
-        return value.isBlank() ? defaultValue : value;
+        return String.valueOf(value);
+    }
+
+    public String getAsString(int i) {
+        return String.valueOf(((Object[]) value)[i]);
     }
 
     public static ArgsOptions forAlias(String alias) {
@@ -63,18 +83,40 @@ public enum ArgsOptions {
         return null;
     }
 
+    private static void error(String message) {
+        throw new IllegalArgumentException(message);
+    }
+
     public static void parse(String... args) {
         ArgsOptions currentOption = null;
-        for (String arg : args) {
-            //parse argument
-            if (arg.startsWith("-")) {
-                String[] argsFound; 
+        int argsRemaining = 0;
 
-                //long form
+        for (String arg : args) {
+            if (argsRemaining > 0) {
+                if (!currentOption.predicate.test(arg)) {
+                    error("Invalid argument for option " + currentOption.name() + ": " + arg);
+                } else {
+                    if (currentOption.argCount == 1) {
+                        currentOption.value = arg;
+                    } else {
+                        ((Object[]) currentOption.value)[currentOption.argCount - argsRemaining] = arg;
+                    }
+                }
+
+                argsRemaining--;
+                if (argsRemaining == 0)
+                    currentOption = null;
+            }
+
+            //check for arg
+            else if (arg.startsWith("-")) {
+                String[] argsFound;
+
+                //check long form
                 if (arg.startsWith("--")) {
                     argsFound = new String[]{arg};
                 }
-                //short form / packed
+                //packed short form
                 else {
                     argsFound = new String[arg.length() - 1];
                     for (int i = 1; i < arg.length(); i++)
@@ -82,25 +124,28 @@ public enum ArgsOptions {
                 }
 
                 //apply the found aliases
+                boolean packed = argsFound.length > 1;
                 for (String alias : argsFound) {
-                    currentOption = forAlias(alias);
-                    if (currentOption == null) {
-                        System.err.println("Unknown command line option: " + alias);
-                    } else if (currentOption.value == null) {
-                        currentOption.value = "true";
+                    ArgsOptions option = forAlias(alias);
+                    if (option == null) {
+                        error("Unknown command line option: " + alias);
+                    } else if (packed && option.argCount > 0) {
+                        error("Option " + alias + " requires arguments and cannot be used in packed form");
+                    } else if (option.argCount == 0) {
+                        option.value = "true";
+                    } else {
+                        currentOption = option;
+                        argsRemaining = option.argCount;
                     }
                 }
             }
-            //apply argument value
-            else if (currentOption != null) {
-                currentOption.value = arg;
-                currentOption = null;
-            }
-            //flow error
-            else {
-                System.err.println("Unexpected command line argument: " + arg);
-            }
+
+            //could not find option
+            else error("Unexpected command line argument: " + arg);
         }
+
+        if (argsRemaining > 0)
+            error("Missing " + argsRemaining + " arguments for option " + currentOption.name());
 
         if (VERSION.getAsBool())
             System.out.println("Cinnamon version " + Version.CLIENT_VERSION);
@@ -109,7 +154,7 @@ public enum ArgsOptions {
             System.out.println("Command Line Options:");
             for (ArgsOptions option : ArgsOptions.values()) {
                 String aliases = String.join(", ", option.aliases);
-                String def = option.defaultValue == null ? "false" : option.defaultValue;
+                String def = option.defaultValue instanceof Object[] arr ? Arrays.toString(arr) : option.defaultValue == null ? "false" : String.valueOf(option.defaultValue);
                 String spacing1 = " ".repeat(Math.max(24 - option.name().length(), 1));
                 String spacing2 = " ".repeat(Math.max(24 - aliases.length(), 1));
 
